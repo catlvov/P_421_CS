f12a578c7431f0b71b58e651c1ac685fc6071e80
User <User@DESKTOP-QHG18FL>
2025-11-27 16:54:23 +0300

    IntroductionToOOP

---
+++ OOP/OOP/Point.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	class Point
+	{
+		double x;
+		double y;
+		/*
+		private:
+		public:
+		protected:
+		internal:
+		protected internal:
+		 */
+		//Get/Set-методы
+		public double GetX()
+		{
+			return x;
+		}
+		public double GetY()
+		{
+			return y;
+		}
+		public void SetX(double x)
+		{
+			this.x = x;
+		}
+		public void SetY(double y)
+		{
+			this.y = y;
+		}
+		//Proprties (Свойства) - это синтаксическая конструкция, 
+		//которая объединяет Get и Set метод для какой-то переменной в классе:
+		//Свойства обычно называют так же как и переменную, но с бльшой буквы
+		//(переменную называют с маленькой).
+		public double X
+		{
+			get
+			{
+				return x;
+			}
+			set
+			{
+				x = value;
+			}
+		}
+		public double Y
+		{
+			get
+			{
+				return y;
+			}
+			set
+			{
+				y = value;
+			}
+		}
+
+		//				Methods:
+		public void Print()
+		{
+			Console.WriteLine($"X = {x}\tY = {y}");
+		}
+	}
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point A = new Point();
+			//A.SetX(2);
+			//A.SetY(3);
+			A.X = 22;	//Свойства позволяют использовать инкапсуляцию как обычные переменные в классе.
+			A.Y = 33;
+			//Console.WriteLine($"{A.GetX()}\t{A.GetY()}");
+			//Assignment - Присваивание
+			//Unassigned - неприсвоенный
+			A.Print();
+		}
+	}
+}
